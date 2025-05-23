bc1a8db3ff63b06455578d2878a09c5fbddd7814
Priyanka-Kuruba <192g1a0535@gmail.com>
2025-05-23 17:11:09 +0530

    assignment1 completed

---
+++ src/assignment/Assignment1_DataTypes.java
@@ -0,0 +1,75 @@
+package assignment;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Assignment-1 | Data types");
+		
+		//1.Temperature of a city in degrees Celsius
+		float temparature=25.5f;
+		
+		//2.Whether a customer has placed an order: true or false
+		boolean order_status=true;
+		
+		//3. Person's phone number: "123-456-7890"
+		String phone_number="123-456-789";
+		
+		//4. Amount of money in a customer's bank account: 1000.50
+		float Amount_of_money=1000.50f;
+				
+		//5. Person's email address: "john.doe@example.com"
+		String email="likhithaprof@gmail.com";
+		
+		//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float latitude=37.7749f;
+		float longitude=-122.4194f;
+				
+		
+		//7. Person's marital status: true or false
+		boolean marital_status=false;
+		
+		//8. Person's occupation: "Software Engineer"
+		String occupation="Software Engineer";
+		
+		//9. Person's favourite colour: "Blue"
+		String fav_color="Blue";
+		
+		//10.Current year: 2023
+		int current_year=2023;
+		
+		//11.Number of followers on a social media platform: 1,000,000
+		long followers=1000000l;
+		
+		//12.Rating of a movie: 7.5
+		float movie_rating=7.5f;
+		
+		//13.Person's blood type: 'A'
+		char blood_type='A';
+		
+		//14.Title of a book: "To Kill a Mockingbird"
+		String book_title="To Kill a Mockingbird";
+		
+		//15.Number of employees in a company: 500
+		int number_of_employees=150;
+		
+		//16.Time of an event: 2:30 PM
+		String time_of_event="2:30 PM";
+		
+		//17.Name of a country: "United States"
+		String Name_of_a_country="United States";
+			
+		//18.Person's eye color: "Brown"
+		String eye_color="Brown";
+		
+		//19.Person's birthplace: "New York City"
+		String Birth_place="New York City";
+		
+		//20.Distance between two cities: 200.5
+		float distance=200.5f;
+		
+		
+
+	}
+
+}
