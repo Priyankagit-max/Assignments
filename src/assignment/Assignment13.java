240df912d975a0ce1f7440720541cb3216539ce4
Priyanka-Kuruba <192g1a0535@gmail.com>
2025-06-07 22:19:51 +0530

    assignment

--- src/assignment/Assignment13.java
+++ src/assignment/Assignment13.java
@@ -3,16 +3,30 @@ package assignment;
 public class Assignment13 {
 
 	public static void main(String[] args) {
-		int number=10;
+		int number=0;
 		boolean is_prime_number=true;
-		for(int i=2;i<number;i++)
+		if(number==1)
 		{
-			if(number%i==0)
-			{
-				is_prime_number=false;
-			}
+			is_prime_number=false;
+			System.out.println((is_prime_number?"prime number":"not prime number"));
 		}
-		System.out.println((is_prime_number?"prime number":"not prime number"));
+		else if(number==0)
+		{
+			System.out.println("0 is neither nor composite");
+		}
+		else
+		{
+			for(int i=2;i<number;i++)
+			{
+				if(number%i==0)
+				{
+					is_prime_number=false;
+				}
+			}
+			System.out.println((is_prime_number?"prime number":"not prime number"));
+		}
+		
+		
 
 	}
 
